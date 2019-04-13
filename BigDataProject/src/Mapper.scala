8a9b894086961ffc8d14ac614738276a8c604fa3
kuusna <kristiina.uusna@gmail.com>
2019-04-13 11:43:41 +0300

    Added distance function

--- BigDataProject/src/Mapper.scala
+++ BigDataProject/src/Mapper.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.Map
+import scala.math.sqrt
 
 object Mapper {
   class Distance(val trainingInstance: List[String], val test: Map[String, List[String]]) {
@@ -6,13 +7,20 @@ object Mapper {
     val class_tr = trainingInstance.last //Class is the last element in the list
     val tr_j = trainingInstance.dropRight(1) //Remove class element from training features
     for (i <- 1 until size) { //for instance of the testing set
-      //var dist: Float = DistanceFunction(trainingInstance, test(i.toString))
+      var dist = DistanceFunction(tr_j, test(i.toString))
+      println(dist)
       //TODO: put result in the Map
     }
   }
   
-  def DistanceFunction(tr_j: List[String], testInstance: List[String]){
-    val tr_i = testInstance.dropRight(1) //Remove class element from testing features
-    //TODO: Calculate distance according to the formula in the article
+  def DistanceFunction(tr_j: List[String], testInstance: List[String]) : Double = {
+    val ts_i = testInstance.dropRight(1) //Remove class element from testing features
+    var sum = 0
+    for (f <- 0 until tr_j.length) {
+      val diff: Double = (tr_j(f).toDouble - ts_i(f).toDouble)
+      val square: Double = (diff * diff)
+      sum +=  square.toInt //TODO: control if converting change anything
+    }
+    return sqrt(sum)
   }
 }
