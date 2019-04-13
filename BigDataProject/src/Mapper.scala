d4cf36fe2d4469dffe1fdaae70663d95226209a5
kuusna <kristiina.uusna@gmail.com>
2019-04-13 10:58:41 +0300

    Add Mapper class :D

--- BigDataProject/src/Mapper.scala
+++ BigDataProject/src/Mapper.scala
@@ -1,5 +1,18 @@
-
+import scala.collection.mutable.Map
 
 object Mapper {
-  //TODO
+  class Distance(val trainingInstance: List[String], val test: Map[String, List[String]]) {
+    val size = test.size
+    val class_tr = trainingInstance.last //Class is the last element in the list
+    val tr_j = trainingInstance.dropRight(1) //Remove class element from training features
+    for (i <- 1 until size) { //for instance of the testing set
+      //var dist: Float = DistanceFunction(trainingInstance, test(i.toString))
+      //TODO: put result in the Map
+    }
+  }
+  
+  def DistanceFunction(tr_j: List[String], testInstance: List[String]){
+    val tr_i = testInstance.dropRight(1) //Remove class element from testing features
+    //TODO: Calculate distance according to the formula in the article
+  }
 }
