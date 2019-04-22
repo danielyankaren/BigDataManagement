b3f0b49051e4c895fdfbbdb351a15d640ca46b48
kuusna <kristiina.uusna@gmail.com>
2019-04-22 20:54:04 +0300

    Creat maven project for Spark and Scala

--- BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
+++ BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
@@ -1,8 +1,15 @@
 
 import scala.collection.mutable.Map
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 
 object Main {
+  var sc: SparkContext = null
+  
   def main(args: Array[String]) {
+      val conf = new SparkConf().setAppName("CoolApp")
+      sc = new SparkContext(conf)
+      
       val data = new DataImport.Import("BigDataProject\\ionosphere.data")
       val train = data.train
       val test = data.test
