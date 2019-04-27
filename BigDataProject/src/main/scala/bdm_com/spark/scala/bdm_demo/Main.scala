6a0a83419fe47e1f73999d3eb24dbd4faa098ac5
kuusna <kristiina.uusna@gmail.com>
2019-04-27 16:06:15 +0300

    Implement the Mapper Function

--- BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
+++ BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
@@ -1,13 +1,16 @@
 package bdm_com.spark.scala.bdm_demo
 
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.EmptyRDD
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
 import scala.util.Random
 import org.apache.spark.sql.types._ 
 import scala.reflect._
 import org.apache.spark.rdd.RDD
 import scala.math.pow
+import scala.math.sqrt
 
 object Main {
   
@@ -38,18 +41,23 @@ object Main {
      //Make training data set splits:
      var splits = 20 //number of splits
      val trainSplits = splitSample(train, splits)
+     var classCol = 34 //class value column id
+           
+     val allMappers = sc.emptyRDD[List[Map[String,List[Any]]]]
      
      for (n <- 0 to trainSplits.size-1) {
        val trainSplit = trainSplits.apply(n) //Training Data Split n
        val combinations = trainSplit.cartesian(test) //1:Training Data Split n, 2:Test Data Set - All combinations
-       //combinations.collect.foreach(println) //to print out the result
-       //TODO: Give combinations to the Mapper       
+       //combinations.collect().foreach(println) //to print out the result
+       val mapper_n = combinations.map{case (a: (Long, Row), b: (Long, Row)) => DistanceFunction(a,b,classCol)}
+       mapper_n.collect().foreach(println) //to print out the n-th mapper output
+       //TODO: append mapper_n to the allMappers
      }
     
      //print(trainSplits.head.collect.foreach(println)) //print out first split
           
      
-     //train.collect().foreach(println)
+//     train.collect().foreach(println)
     
     
       //val data = new DataImport.Import("ionosphere.data")
@@ -94,4 +102,31 @@ object Main {
     }
   }
 }
+  
+  def DistanceFunction(trainInstance:(Long, Row), testInstance:(Long, Row), classCol: Int) : List[Map[String,List[Any]]]  = {
+    val class_tr = trainInstance._2(classCol)
+    var sum = 0.0
+    var dist_map = Map[String, List[Any]]()
+    var dist_map_list = List[Map[String, List[Any]]]()
+//    val ex = "notNone"
+    for (k <- 0 until testInstance._2.size-1) {
+//      try{val trj: Double = trainInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
+//      try{val tsi: Double = testInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
+        val trj: Double = trainInstance._2.getString(k).toDouble
+        val tsi: Double = testInstance._2.getString(k).toDouble
+      
+//      if(trj != None && tsi != None){
+        val diff: Double = trj - tsi
+        val square: Double = (diff * diff)
+        sum +=  square
+//      }
+    }
+    val dist_ij: Double = sqrt(sum)
+    var lst = List(trainInstance._1,dist_ij,class_tr)
+    dist_map = Map(testInstance._1.toString -> lst)
+    dist_map_list ::= dist_map
+    
+    return dist_map_list
+}
+   
 }
