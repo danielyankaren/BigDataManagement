dafa7f241fbec0e465580863689bd8cdd528f1da
kuusna <kristiina.uusna@gmail.com>
2019-04-27 23:09:07 +0300

    Put Mapper part in the Mapper class

--- BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
+++ BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
@@ -19,7 +19,6 @@ object Main {
     //IF SPARK DOESN'T WORK, YOU SHOULD USE THIS
     //More information about that what you should install:
     //https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-tips-and-tricks-running-spark-windows.html
-    
      val conf = new SparkConf()
             .setMaster("local[2]")
              .setAppName("BDMProject")
@@ -41,57 +40,13 @@ object Main {
      //Make training data set splits:
      var splits = 20 //number of splits
      val trainSplits = splitSample(train, splits)
-     var classCol = 34 //class value column id
-           
-     var allMappers = sc.emptyRDD[Map[String,List[Any]]]
+     var classCol: Integer = 34 //class value column id
      
-     for (n <- 0 to trainSplits.size-1) {
-       val trainSplit = trainSplits.apply(n) //Training Data Split n
-       val combinations = trainSplit.cartesian(test) //1:Training Data Split n, 2:Test Data Set - All combinations
-       //combinations.collect().foreach(println) //to print out the result
-       val mapper_n = combinations.map{case (a: (Long, Row), b: (Long, Row)) => DistanceFunction(a,b,classCol)}
-       //mapper_n.collect().foreach(println) //to print out the n-th mapper output
-       allMappers = allMappers.union(mapper_n)
-     }
-    //print(allMappers.take(1).head)
-     //print(trainSplits.head.collect.foreach(println)) //print out first split
-          
+     var allMappers = sc.emptyRDD[Map[String,List[Any]]] //Value for all the Mapper's output
      
-//     train.collect().foreach(println)
-    
-    
-      //val data = new DataImport.Import("ionosphere.data")
-      //val train = data.train
-      //val test = data.test
-      //var splits_n = 20 //how many training data splits need
-      //val train_split = Map[String, List[String]]()
-      //for (j <- 1 until train.size) {
-      //  train_split += (j.toString -> train(j.toString))
-      //   if(j % splits_n == 0){
-      //     val mapper = new Mapper.Distance(train_split, test, j)
-      //     train_split.clear
-      //   }
-      //}
-      //if(!train_split.isEmpty){
-      // val mapper = new Mapper.Distance(train_split, test, train.size)
-      // train_split.clear
-      //}
-   
-      //TODO: we need to use this somehow:
-      //   val trainRdd: RDD[(String, LabeledPoint)]  = sc
-      // Convert Map to Seq so it can passed to parallelize
-      //  .parallelize(train.toSeq)
-      //  .map{case (id, (labelInt, values)) => {
-
-      // Convert nested map to Seq so it can be passed to Vector
-      //   val features = Vectors.sparse(nFeatures, values.toSeq)
-
-      // Convert label to Double so it can be used for LabeledPoint
-      //   val label = labelInt.toDouble
-
-      //  (id, LabeledPoint(label, features))
-      // }
-
+     allMappers = new Mapper.Distance(trainSplits, test, classCol, allMappers).result
+     //print(allMappers.take(1).head)
+     
   }
   
   def splitSample[T :ClassTag](rdd: RDD[T], n: Int, seed: Long = 42): Seq[RDD[T]] = {
@@ -103,30 +58,5 @@ object Main {
   }
 }
   
-  def DistanceFunction(trainInstance:(Long, Row), testInstance:(Long, Row), classCol: Int) : Map[String,List[Any]]  = {
-    val class_tr = trainInstance._2(classCol)
-    var sum = 0.0
-    var dist_map = Map[String, List[Any]]()
-    //var dist_map_list = List[Map[String, List[Any]]]()
-//    val ex = "notNone"
-    for (k <- 0 until testInstance._2.size-1) {
-//      try{val trj: Double = trainInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
-//      try{val tsi: Double = testInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
-        val trj: Double = trainInstance._2.getString(k).toDouble
-        val tsi: Double = testInstance._2.getString(k).toDouble
-      
-//      if(trj != None && tsi != None){
-        val diff: Double = trj - tsi
-        val square: Double = (diff * diff)
-        sum +=  square
-//      }
-    }
-    val dist_ij: Double = sqrt(sum)
-    val lst = List(trainInstance._1,dist_ij,class_tr)
-    dist_map = Map(testInstance._1.toString -> lst)
-    //dist_map_list ::= dist_map
-    
-    return dist_map
-}
    
 }
--- BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Mapper.scala
+++ BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Mapper.scala
@@ -1,35 +1,54 @@
 package bdm_com.spark.scala.bdm_demo
 
-import scala.collection.mutable.Map
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.EmptyRDD
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
+import scala.util.Random
+import org.apache.spark.sql.types._ 
+import scala.reflect._
+import org.apache.spark.rdd.RDD
+import scala.math.pow
 import scala.math.sqrt
 
-object Mapper {
-  class Distance(val train: Map[String, List[String]], val test: Map[String, List[String]], val t_stop: Int){
-    val size = test.size
-    var t_start = t_stop-19   
-    var dist_map = Map[String, List[Any]]()
-    var dist_map_list = List[Map[String, List[Any]]]()
-    for (i <- 1 until size) { //for instance of the testing set
-      for (j <- t_start to t_stop){ //for instance of the training set
-        val class_tr = train(j.toString).last //Class is the last element in the list
-        val tr_j = train(j.toString).dropRight(1) //Remove class element from training features
-        var dist = DistanceFunction(tr_j, test(i.toString))
-        var lst = List(train.keys.head,dist,class_tr)
-        dist_map = Map(test.keysIterator.toList(i) -> lst)
-        dist_map_list ::= dist_map
-      }
-    }
+object Mapper{
+  class Distance(trainSplits: Seq[RDD[(Long, Row)]]  , test: RDD[(Long, org.apache.spark.sql.Row)] , classCol: Integer, allMappers: RDD[Map[String,List[Any]]]) extends Serializable{
+     var result: RDD[Map[String,List[Any]]] = allMappers
+     for (n <- 0 to trainSplits.size-1) {
+       val trainSplit = trainSplits.apply(n) //Training Data Split n
+       val combinations = trainSplit.cartesian(test) //1:Training Data Split n, 2:Test Data Set - All combinations
+       //combinations.collect().foreach(println) //to print out the result
+       val mapper_n = combinations.map{case (a: (Long, Row), b: (Long, Row)) => DistanceFunction(a,b,classCol)}
+       //mapper_n.collect().foreach(println) //to print out the n-th mapper output
+       result = result.union(mapper_n)
+     }
+     //print(result.take(1).head)
+     //print(trainSplits.head.collect.foreach(println)) //print out first split
   }
   
-  def DistanceFunction(tr_j: List[String], testInstance: List[String]) : Double = {
-    val ts_i = testInstance.dropRight(1) //Remove class element from testing features
+  private def DistanceFunction(trainInstance:(Long, Row), testInstance:(Long, Row), classCol: Int) : Map[String,List[Any]]  = {
+    val class_tr = trainInstance._2(classCol)
     var sum = 0.0
-    for (f <- 0 until tr_j.length) {
-      val diff: Double = (tr_j(f).toDouble - ts_i(f).toDouble)
-      val square: Double = (diff * diff)
-      sum +=  square
+    var dist_map = Map[String, List[Any]]()
+//    val ex = "notNone"
+    for (k <- 0 until testInstance._2.size-1) {
+//      try{val trj: Double = trainInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
+//      try{val tsi: Double = testInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
+        val trj: Double = trainInstance._2.getString(k).toDouble
+        val tsi: Double = testInstance._2.getString(k).toDouble
+      
+//      if(trj != None && tsi != None){
+        val diff: Double = trj - tsi
+        val square: Double = (diff * diff)
+        sum +=  square
+//      }
     }
-    return sqrt(sum)
-  }
-  
+    val dist_ij: Double = sqrt(sum)
+    val lst = List(trainInstance._1,dist_ij,class_tr)
+    dist_map = Map(testInstance._1.toString -> lst)    
+    return dist_map
 }
+}
+
+  
