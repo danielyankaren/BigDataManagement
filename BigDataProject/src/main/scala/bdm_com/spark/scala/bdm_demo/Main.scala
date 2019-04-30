b12a176e5119d1199f7f7e9ffc1682879cf811b5
Indrek Polding <indrek.polding@gmail.com>
2019-04-30 17:27:11 +0300

    Code has error, but tried some conversion

--- BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
+++ BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Main.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.Row
 import scala.util.Random
-import org.apache.spark.sql.types._ 
+import org.apache.spark.sql.types._
 import scala.reflect._
 import org.apache.spark.rdd.RDD
 import scala.math.pow
@@ -30,25 +30,62 @@ object Main {
 
      // Randomly splitting the data into train/test sets.
      val Array(data1, data2) = rdd.randomSplit(Array(0.8, 0.2))
- 
+
      //The input of the Mapper function is a <Key, Value> vector
      //key : the record id of the training instance
      //value: the set of feature values of the training instance
      val train = data1.zipWithIndex.map(_.swap)
      val test = data2.zipWithIndex.map(_.swap)
-          
      //Make training data set splits:
      var splits = 20 //number of splits
      val trainSplits = splitSample(train, splits)
      var classCol: Integer = 34 //class value column id
      
-     var allMappers = sc.emptyRDD[Map[String,List[Any]]] //Value for all the Mapper's output
-     
-     allMappers = new Mapper.Distance(trainSplits, test, classCol, allMappers).result
-     //print(allMappers.take(1).head)
+     var allMappers = sc.emptyRDD[(String, Row)] //Value for all the Mapper's output
+    var result: RDD[(String, Row)] = allMappers
+    for (n <- 0 to trainSplits.size-1) {
+      val trainSplit = trainSplits.apply(n) //Training Data Split n
+      val combinations = trainSplit.cartesian(test) //1:Training Data Split n, 2:Test Data Set - All combinations
+      val trainInstance = combinations.take(1).head._1
+      val testInstance = combinations.take(1).head._2
+      val class_tr = trainInstance._2(classCol)
+      //combinations.collect().foreach(println) //to print out the result
+      val mapper_n = combinations.map{case (a: (Long, Row), b: (Long, Row)) => DistanceFunction(a,b,classCol)}
+      //mapper_n.collect().foreach(println) //to print out the n-th mapper output
+
+      result = result.union(mapper_n.take(1).head)
+    }
+
+//     allMappers = new Mapper.Distance(trainSplits, test, classCol, allMappers).result
+
+    //print(allMappers.take(1).head)
      
   }
-  
+  private def DistanceFunction(trainInstance:(Long, Row), testInstance:(Long, Row), classCol: Int) : RDD[(String, Row)]  = {
+    val class_tr = trainInstance._2(classCol)
+    var sum = 0.0
+    //    val ex = "notNone"
+    for (k <- 0 until testInstance._2.size-1) {
+      if(k!=classCol){
+        //      try{val trj: Double = trainInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
+        //      try{val tsi: Double = testInstance._2.getString(5).toDouble}catch{case e: NumberFormatException => ex=="None"}
+        val trj: Double = trainInstance._2.getString(k).toDouble
+        val tsi: Double = testInstance._2.getString(k).toDouble
+
+        //      if(trj != None && tsi != None){
+        val diff: Double = trj - tsi
+        val square: Double = (diff * diff)
+        sum +=  square
+        //      }
+      }
+    }
+    val dist_ij: Double = sqrt(sum)
+    val lst = Row(trainInstance._1,dist_ij,class_tr)
+    val ts = testInstance._1.toString
+    val dist_map = sc.parallelize(List(lst))
+    val mapper_n = dist_map.map(a => (ts,a))
+    return mapper_n
+  }
   def splitSample[T :ClassTag](rdd: RDD[T], n: Int, seed: Long = 42): Seq[RDD[T]] = {
   Vector.tabulate(n) { j =>
     rdd.mapPartitions { data =>
