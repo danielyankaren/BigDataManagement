25133913a74d1e0d0d6a484940422cf081a0a0f9
kuusna <kristiina.uusna@gmail.com>
2019-05-20 23:55:05 +0300

    Calculated the centroids of the instances.

--- BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Reducer.scala
+++ BigDataProject/src/main/scala/bdm_com/spark/scala/bdm_demo/Reducer.scala
@@ -14,18 +14,38 @@ object Reducer{
         
     val groupedTopZ = topK.mapValues(Values => groupByClass(Values))
     
+    //calculating distance means by class:
+    val distanceMeansByClass = groupedTopZ.mapValues(Values => calcDistanceMean(Values))
+    
     //topK.collect.foreach(println)
-    groupedTopZ.foreach(println)
+    distanceMeansByClass.foreach(println)
         
     def groupByClass(lists: List[List[Double]]): Map[Double,List[List[Double]]]   = { 
       
-      //Group values by traing data class values:
+      //Group values by training data class values:
       val groupByClass = lists.groupBy(list => list.apply(2))
       //separating Z instances from each of the classes:
       val topZ = groupByClass.mapValues(Values => Values.take(Z))
       topZ
       
     }
+    
+    def calcDistanceMean(input: Map[Double,List[List[Double]]] ): Map[Double,Double]   = { 
+
+      val distanceMeans = input.mapValues(Values => mean(Values)) 
+      distanceMeans      
+      
+    }
+    
+    def mean(classBasedDistances: List[List[Double]]): Double ={
+      
+      //Put all the distance values in the one Map
+      val distances = classBasedDistances.map(list => list.apply(1))
+      var sum = distances.sum
+      val mu = sum/distances.size
+      mu
+      
+     }
         
     
     // TODO:Implement the reducer part
